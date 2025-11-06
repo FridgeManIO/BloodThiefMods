b9e7e6b1de4bac3d9a74e4e12a4b65be25898d21
FridgeManIO <sg7055664@gmail.com>
2025-11-06 16:28:24 +0000

    Update main_screen_override.gd

--- fridge-unreleasedMapLoader/main_screen_override.gd
+++ fridge-unreleasedMapLoader/main_screen_override.gd
@@ -2,16 +2,12 @@ extends "res://scripts/ui/main_screen.gd"
 
 func get_map_path(file)-> String:
     var f = FileAccess.open(file, FileAccess.READ)
-    while true:
+    while not f.eof_reached():
         var line = f.get_line()
         if line[0]!='#':
-            f.close()
             return line
-
-
     f.close()
     return ""
-
 
 func _on_custom_maps_button_pressed() -> void :
     var map_path = get_map_path("res://levels.txt")
