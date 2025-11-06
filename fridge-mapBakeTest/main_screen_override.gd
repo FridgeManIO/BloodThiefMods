41daa86a25666516bf0ac86c1bcb974ae343d33d
FridgeManIO <sg7055664@gmail.com>
2025-11-06 16:11:56 +0000

    Added map loader

---
+++ fridge-mapBakeTest/main_screen_override.gd
@@ -0,0 +1,20 @@
+extends "res://scripts/ui/main_screen.gd"
+
+func get_map_path(file)-> String:
+    var f = FileAccess.open(file, FileAccess.READ)
+    while true:
+        var line = f.get_line()
+        if line[0]!='#':
+            return line
+
+
+    f.close()
+    return ""
+
+
+func _on_custom_maps_button_pressed() -> void :
+    var map_path = get_map_path("res://levels.txt")
+    if map_path == "":
+        GameManager.change_level_scene("res://scenes/levels/experimental/dummy_runtime_map_build.tscn")
+    print("Loading level"+map_path)
+    GameManager.change_level_scene(map_path)
